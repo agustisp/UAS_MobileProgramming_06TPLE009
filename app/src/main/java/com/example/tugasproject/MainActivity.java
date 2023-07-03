3514bdd74f14bdd061adad30757dcf92091dc6bb
agustisp <agustisetiabudipratama@gmail.com>
2023-07-03 22:29:33 +0700

    Ini sudah diupdate di java nya untuk mengfunsikan type checkbox dari show in my password sehingga pada password bisa di tampilkan dan disembunyikan dengan function yang terdapat di file MainActivity.java

--- app/src/main/java/com/example/tugasproject/MainActivity.java
+++ app/src/main/java/com/example/tugasproject/MainActivity.java
@@ -4,23 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     Button Login_button;
+    EditText passwordEditText;
+    CheckBox showPasswordCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Login_button =(Button) findViewById(R.id.Login_button);
+        Login_button = (Button) findViewById(R.id.Login_button);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        showPasswordCheckBox = (CheckBox) findViewById(R.id.showPasswordCheckBox);
 
         Login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent BukaActDua = new Intent(getApplicationContext(), MainActivity2.class);
                 startActivity(BukaActDua);
+            }
+        });
+
+        showPasswordCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked) {
+                    // Tampilkan password
+                    passwordEditText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                } else {
+                    // Sembunyikan password
+                    passwordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
             }
         });
     }
