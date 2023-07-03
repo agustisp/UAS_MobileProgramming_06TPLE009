978f03ed88e2e1efacf5c95365902fc0a6247986
agustisp <agustisetiabudipratama@gmail.com>
2023-07-03 21:38:42 +0700

    Project Form Login first step gais UTS Mobile Programming

---
+++ app/src/main/java/com/example/tugasproject/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.tugasproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button Login_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Login_button =(Button) findViewById(R.id.Login_button);
+
+        Login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent BukaActDua = new Intent(getApplicationContext(), MainActivity2.class);
+                startActivity(BukaActDua);
+            }
+        });
+    }
+}
